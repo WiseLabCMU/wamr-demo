7caa38c81ef89b3b347b7a34faf80806d384a539
Nuno <3504501+nampereira@users.noreply.github.com>
2019-09-23 02:54:04 -0400

    Docker using parameters (runtime uuid, ip,port) and sending start event

--- docker/start-bridged-runtime.sh
+++ docker/start-bridged-runtime.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+# uses RT_UUID, IP_ADDR, $PORT variables that can be passed when starting the container with the -e flag
 
 cd out/
 
-# generate uuid into config.ini
-sed -i 's/uuid=/uuid='$(cat /proc/sys/kernel/random/uuid)'; /g' config.ini
+if [ -z "$RT_UUID" ]
+then
+    RT_UUID=$(cat /proc/sys/kernel/random/uuid)
+fi
+
+MQTT_SRV=$(cat config.ini  | grep server_address | cut -d'=' -f2)
+TOPIC_PREFIX=$(cat config.ini | grep topic-prefix | cut -d'=' -f2)
+
+# sends a runtime start message on behaf of the runtime (which will still send its own, but the second message to the same runtime uuid is ignored)
+mosquitto_pub -h $MQTT_SRV -t $TOPIC_PREFIX'/'$RT_UUID -m '{ "id":"'$RT_UUID'", "label": "'$RT_UUID'", "cmd": "rt-start", "address": "'$IP_ADDR'", "port":"'$PORT'"}'
+
+# insert uuid into config.ini
+sed -i 's/uuid=/uuid='$RT_UUID'; /g' config.ini
 
 ./http_upload &
 ./runtime -s &
