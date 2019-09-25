60691e80cfc21f3f843413314d282a1f77854ed0
Nuno <3504501+nampereira@users.noreply.github.com>
2019-09-25 18:13:32 -0400

    Dockerfiles updates

--- docker-runtime.sh
+++ docker-runtime.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+# docker-runtime <port> <uuid>
 
 chmod +x docker/start-bridged-runtime.sh
-#[[ $# -eq 1 ]] && docker run -d -v ${PWD}:/wamr-demo -p $1:8000 --rm --name wamr-demo-container npereira/wamr-demo /wamr-demo/docker/start-bridged-runtime.sh 
-#[[ $# -eq 0 ]] && docker run -d -v ${PWD}:/wamr-demo -p 8000:8000 --rm --name wamr-demo-container npereira/wamr-demo /wamr-demo/docker/start-bridged-runtime.sh 
 
-[[ $# -eq 1 ]] && docker run -d -p $1:8000 --rm --name wamr-demo-container npereira/wamr-demo 
-[[ $# -eq 0 ]] && docker run -d -p 8000:8000 --rm --name wamr-demo-container npereira/wamr-demo 
+RUNTIME_PORT=${1:-8000}
+RUNTIME_UUID=${2:-$(cat /proc/sys/kernel/random/uuid)}
+RUNTIME_ADDR=${3:-spatial.andrew.cmu.edu}
+
+printf "Runtime port=%s; uuid=%s; addr=%s\n" $RUNTIME_PORT $RUNTIME_UUID $RUNTIME_ADDR
+
+docker run -d -p $RUNTIME_PORT:8000 -e RT_UUID=$RUNTIME_UUID -e ADDR=$RUNTIME_ADDR -e PORT=$RUNTIME_PORT --rm --name wamr-demo-container npereira/wamr-demo 
--- docker/start-bridged-runtime.sh
+++ docker/start-bridged-runtime.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# uses RT_UUID, IP_ADDR, $PORT variables that can be passed when starting the container with the -e flag
+# uses RT_UUID, IP_ADDR, PORT variables that can be passed when starting the container with the -e flag
 
 cd out/
 
@@ -8,14 +8,15 @@ then
     RT_UUID=$(cat /proc/sys/kernel/random/uuid)
 fi
 
+# get mqtt server and topic prefix from config.ini
 MQTT_SRV=$(cat config.ini  | grep server_address | cut -d'=' -f2)
 TOPIC_PREFIX=$(cat config.ini | grep topic-prefix | cut -d'=' -f2)
 
 # sends a runtime start message on behaf of the runtime (which will still send its own, but the second message to the same runtime uuid is ignored)
-mosquitto_pub -h $MQTT_SRV -t $TOPIC_PREFIX'/'$RT_UUID -m '{ "id":"'$RT_UUID'", "label": "'$RT_UUID'", "cmd": "rt-start", "address": "'$IP_ADDR'", "port":"'$PORT'"}'
+mosquitto_pub -h $MQTT_SRV -t $TOPIC_PREFIX'/'$RT_UUID -m '{ "id":"'$RT_UUID'", "label": "'$RT_UUID'", "cmd": "rt-start", "address": "'$ADDR'", "port":"'$PORT'"}'
 
 # insert uuid into config.ini
-sed -i 's/uuid=/uuid='$RT_UUID'; /g' config.ini
+sed -i 's/uuid=/uuid='$RT_UUID' ; /g' config.ini
 
 ./http_upload &
 ./runtime -s &
