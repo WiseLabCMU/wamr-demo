c31c27a7f93cba7b2d032c1a22aadc55ffba7aab
Nuno <3504501+nampereira@users.noreply.github.com>
2019-09-30 20:32:18 -0400

    Fixes do docker files

--- docker-runtime.sh
+++ docker-runtime.sh
@@ -8,4 +8,4 @@ RUNTIME_UUID=${2:-$(cat /proc/sys/kernel/random/uuid)}
 RUNTIME_ADDR=${3:-spatial.andrew.cmu.edu}
 
 printf "Runtime port=%s; uuid=%s; addr=%s\n" $RUNTIME_PORT $RUNTIME_UUID $RUNTIME_ADDR
-docker run -d -it -v $PWD:/wamr -p $RUNTIME_PORT:8000 -e RT_UUID=$RUNTIME_UUID -e ADDR=$RUNTIME_ADDR -e PORT=$RUNTIME_PORT --name wamr-demo-container-$RUNTIME_PORT npereira/wamr-demo
+docker run -d -it -p $RUNTIME_PORT:8000 -e RT_UUID=$RUNTIME_UUID -e ADDR=$RUNTIME_ADDR -e PORT=$RUNTIME_PORT --name wamr-demo-container-$RUNTIME_PORT npereira/wamr-demo
